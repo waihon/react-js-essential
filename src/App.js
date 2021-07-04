b4e415acaf450a1ad0952a8d233c56301909ade5
Waihon Yew <yewwaihon@gmail.com>
2021-07-04 17:05:31 +0800

    Implement Header component and replace h1 with that

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
+function Header() {
+  return(
+    <header>
+      <h1>Eve's Kitchen</h1>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Header</h1>
+      <Header />
       <h2>Main</h2>
       <h3>Footer</h3>
     </div>
