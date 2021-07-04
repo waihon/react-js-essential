990afe108c9eecaea0f0686738ed62262768a268
Waihon Yew <yewwaihon@gmail.com>
2021-07-04 16:58:14 +0800

    Revert index.js to App component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App";
 
 ReactDOM.render(
-  <ul>
-    <li>Monday</li>
-    <li>Tuesday</li>
-    <li>Wednesday</li>
-  </ul>,
+  <App />,
   document.getElementById('root')
 );
